25995fbcea5ef776bc423c337866a3c905da8bd9
Omar Aguirre <omersguchigu@gmail.com>
2020-05-17 19:49:44 -0500

    generla updates

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,5 +8,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar main -c ~/.config/polybar/config.ini &
+# Launch bar
+# polybar main -c ~/.config/polybar/config.ini &
+
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main -c ~/.config/polybar/config.ini &
+  done
+else
+  polybar --reload main -c ~/.config/polybar/config.ini &
+fi
